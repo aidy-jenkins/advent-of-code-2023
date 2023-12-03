db82d5db5d33c7661b07b0db06daad6b896693cd
aidy-jenkins <git@aidyj.uk>
2023-12-03 23:41:54 +0000

    day 1 solution

--- spec/Puzzle.spec.ts
+++ spec/Puzzle.spec.ts
@@ -7,6 +7,12 @@ describe("Puzzle", () => {
             expect(Puzzle.getFirstDigit("a1a2a3")).toBe(1);
             expect(Puzzle.getFirstDigit("sometext7")).toBe(7);
         });
+
+        it("should return the first number spelled out or numeric char", () => {
+            expect(Puzzle.getFirstDigit("1two3")).toBe(1);
+            expect(Puzzle.getFirstDigit("two3four")).toBe(2);
+            expect(Puzzle.getFirstDigit("seveneightnine")).toBe(7);
+        });
     });
 
     describe("getLastDigit", () => {
@@ -15,11 +21,29 @@ describe("Puzzle", () => {
             expect(Puzzle.getLastDigit("a1a2a3")).toBe(3);
             expect(Puzzle.getLastDigit("sometext7")).toBe(7);
         });
+
+        it("should return the last number spelled out or numeric char", () => {
+            expect(Puzzle.getLastDigit("1two3")).toBe(3);
+            expect(Puzzle.getLastDigit("two3four")).toBe(4);
+            expect(Puzzle.getLastDigit("seveneightnine")).toBe(9);
+        });
     });
 
     describe("solve", () => {
         it("should return the summation of first and last digits by line in a block of text", () => {
             expect(Puzzle.solve(solutionExample)).toBe(142);
+        });
+
+        it("should return the summation of the first and last numbers in a block of text, with numbers spelled out", () => {
+            expect(Puzzle.solve(solutionExampleWords)).toBe(281);
+        });
+
+        it("should handle multiple words overlapping", () => {
+            expect(Puzzle.solve("twone")).toBe(21);
+        });
+
+        it("should handle the same word being repeated at start and end", () => {
+            expect(Puzzle.solve("oneone")).toBe(11);
         })
     })
 });
@@ -28,5 +52,13 @@ const solutionExample =
 `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet
-`;
+treb7uchet`;
+
+const solutionExampleWords = 
+`two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
--- src/Puzzle.ts
+++ src/Puzzle.ts
@@ -1,11 +1,36 @@
- const numericChars = Array(10).fill(0).map((_, idx) => idx).reduce((prev, curr) => { prev[curr] = curr; return prev; }, Object.create(null));
- 
- export const getFirstDigit = (content: Iterable<string>) => Array.from(content).map(char => numericChars[char]).find(x => x != null) as number;
- export const getLastDigit = (content: Iterable<string>) => getFirstDigit(Array.from(content).reverse());
+import {linq} from "linq-fast";
+
+const numericWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+ const numericChars = Array(10).fill(0).map((_, idx) => idx.toString());
+ const numericSearch = numericWords.concat(numericChars);
+
+ const numericDictionary = Object.create(null) as { [key: string]: number; };
+ numericChars.reduce((prev, curr, idx) => { prev[curr] = idx; return prev; }, numericDictionary);
+ numericWords.reduce((prev, curr, idx) => { prev[curr] = idx; return prev; }, numericDictionary);
+
+ export const getFirstDigit = (line: string) => { 
+    let itemIndices = numericSearch.map(searchItem => [searchItem, line.indexOf(searchItem)] as [string, number]);
+
+    return linq(itemIndices)
+    .where(([_, index]) => index >= 0)
+    .orderBy(([_, index]) => index)
+    .select(([item, _]) => numericDictionary[item])
+    .first();
+ };
+
+ export const getLastDigit = (line: string) => {
+    let itemIndices = numericSearch.map(searchItem => [searchItem, line.lastIndexOf(searchItem)] as [string, number]);
+
+    return linq(itemIndices)
+    .where(([_, index]) => index >= 0)
+    .orderByDescending(([_, index]) => index)
+    .select(([item, _]) => numericDictionary[item])
+    .first();
+ }
 
  export const solve = (content: string) => {
     let lines = content.split('\n').filter(x => Boolean(x));
-
+    
     let digits = lines.map(line => [getFirstDigit(line), getLastDigit(line)]).map(([first, last]) => (first * 10) + last);
-    return digits.reduce((prev, curr) => prev + curr, 0);
+    return linq(digits).sum();
  };
